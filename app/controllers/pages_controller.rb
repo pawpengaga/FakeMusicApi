ef8d62eff1efd1d65424237b6855137f94882bcf
Erick Rivera <yomellamoerick.771@gmail.com>
2023-06-22 21:28:11 -0400

    the api now has a view

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ActionController::Base
+  def index
+    render layout: 'application'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :music_albums
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'pages#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
